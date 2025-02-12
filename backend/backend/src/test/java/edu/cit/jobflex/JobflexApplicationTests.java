85877facca0bdd828e97ac420e79f4a58d27de8c
LAB14_USER ON WS09 <L14X08W09@citulab.net>
2025-02-12 09:09:00 +0800

    added backend

---
+++ backend/backend/src/test/java/edu/cit/jobflex/JobflexApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.jobflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
