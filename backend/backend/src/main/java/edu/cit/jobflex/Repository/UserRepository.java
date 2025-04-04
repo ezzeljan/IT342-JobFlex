6065f99acc64782e44ad8c24e44fdc1e9d08df32
ezzeljan <ezzelfrancisco95@gmail.com>
2025-04-04 13:39:13 +0800

    create vite

--- backend/backend/src/main/java/edu/cit/jobflex/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.cit.jobflex.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.cit.jobflex.Entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-	UserEntity findByEmail(String email);
-}
